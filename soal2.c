622625b559deab6af88177c8faff3561669f655f
Michael Reynaldi Tamara <117028090+Avereyy@users.noreply.github.com>
2026-05-06 07:45:39 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int level = 0;
+    int index = 0;
+
+    while (index < N) {
+        int nodes_in_level = pow(2, level);
+
+        printf("LEVEL %d: ", level);
+
+        for (int i = 0; i < nodes_in_level && index < N; i++) {
+            printf("%d", arr[index]);
+            index++;
+
+            if (i != nodes_in_level - 1 && index < N)
+                printf(" ");
+        }
+
+        printf("\n");
+        level++;
+    }
+
     return 0;
 }
